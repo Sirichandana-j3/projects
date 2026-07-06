831b2454a0fa7816ae4555d669723883b9cf5bbf
Siri Reddy <sirireddy7133@gmail.com>
2026-07-06 18:36:57 +0530

    Add files via upload
    A responsive Basic Calculator built using HTML, CSS, and JavaScript. It performs standard arithmetic operations including addition, subtraction, multiplication, and division through an interactive and user-friendly interface. This project demonstrates fundamental front-end development concepts, DOM manipulation, event handling, and responsive UI design.

---
+++ calculator/script.js
@@ -0,0 +1,16 @@
+const display=document.getElementById("display");
+        function addToDisplay(input){
+            display.value += input;
+        }
+        function calculate(){
+            try{
+                display.value=eval(display.value);
+
+            }catch(error){
+                display.value="Error";
+            }
+            
+        }
+        function clear1(){
+            display.value="";
+        }
